559c78e96485e1ad2d37c7590a0944be2c0fed14
Maslan22 <123606263+Maslan22@users.noreply.github.com>
2023-08-07 21:08:13 +0000

    Add files via upload

---
+++ ORRS-PHP/complaints.php
@@ -0,0 +1,129 @@
+<!--Start Server side code to give us and hold session-->
+<?php
+  session_start();
+  include('assets/inc/config.php');
+  include('assets/inc/checklogin.php');
+  check_login();
+  $aid=$_SESSION['pass_id'];
+?>
+<!--End Server side scriptiing-->
+<!DOCTYPE html>
+<html lang="en">
+<!--HeAD-->
+  <?php include('assets/inc/head.php');?>
+ <!-- end HEAD--> 
+  <body>
+    <div class="be-wrapper be-fixed-sidebar">
+    <!--navbar-->
+      <?php include('assets/inc/navbar.php');?>
+      <!--End navbar-->
+      <!--Sidebar-->
+      <?php include('assets/inc/sidebar.php');?>
+      <!--End Sidebar-->
+
+      <div class="be-content">
+     
+      <div class="page-wrapper">
+            <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+    <div class="row">
+        <ol class="breadcrumb">
+            <li><a href="#">
+                    <em class="fa fa-home"></em>
+                </a></li>
+            <li class="active">Feedback</li>
+        </ol>
+    </div><!--/.row-->
+
+    
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                
+                <div class="panel-body">
+                
+                    <form role="form"  data-toggle="validator" method="post" action="complaints.php">
+                        <div class="row">
+                            <div class="form-group col-lg-6">
+                                <label>Name</label>
+                                <input type="text" class="form-control" placeholder="Name" name="complainant_name" required>
+                                <div class="help-block with-errors"></div>
+                            </div>
+
+                            <div class="form-group col-lg-6">
+                                <label>Feedback Type</label>
+                                <input type="text" class="form-control" placeholder="Feedback Type" name="complaint_type" required>
+                                <div class="help-block with-errors"></div>
+                            </div>
+
+                            <div class="form-group col-lg-12">
+                                <label>Giive your feedback</label>
+                                <textarea class="form-control" name="complaint" placeholder="Feedback" required></textarea>
+                            </div>
+
+                        </div>
+
+                        <button type="submit" class="btn btn-lg btn-success" name="createComplaint" style="border-radius:0%">Submit</button>
+                        
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+           <?php
+// Connect to the database
+$conn = mysqli_connect("localhost", "root", "", "compliant");
+
+// Check for connection errors
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Check if the form has been submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get the values from the form
+    $complainant_name = $_POST["complainant_name"];
+    $complaint_type = $_POST["complaint_type"];
+    $complaint = $_POST["complaint"];
+  
+
+    // Insert the data into the database
+    $sql = "INSERT INTO complaint (complainant_name, complaint_type, complaint) VALUES ('$complainant_name', '$complaint_type', '$complaint')";
+    if (mysqli_query($conn, $sql)) {
+        echo "Complaint submitted successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+}
+
+// Close the database connection
+mysqli_close($conn);
+?>
+    <script src="assets/lib/jquery/jquery.min.js" type="text/javascript"></script>
+    <script src="assets/lib/perfect-scrollbar/js/perfect-scrollbar.min.js" type="text/javascript"></script>
+    <script src="assets/lib/bootstrap/dist/js/bootstrap.bundle.min.js" type="text/javascript"></script>
+    <script src="assets/js/app.js" type="text/javascript"></script>
+    <script src="assets/lib/datatables/datatables.net/js/jquery.dataTables.js" type="text/javascript"></script>
+    <script src="assets/lib/datatables/datatables.net-bs4/js/dataTables.bootstrap4.js" type="text/javascript"></script>
+    <script src="assets/lib/datatables/datatables.net-buttons/js/dataTables.buttons.min.js" type="text/javascript"></script>
+    <script src="assets/lib/datatables/datatables.net-buttons/js/buttons.flash.min.js" type="text/javascript"></script>
+    <script src="assets/lib/datatables/jszip/jszip.min.js" type="text/javascript"></script>
+    <script src="assets/lib/datatables/pdfmake/pdfmake.min.js" type="text/javascript"></script>
+    <script src="assets/lib/datatables/pdfmake/vfs_fonts.js" type="text/javascript"></script>
+    <script src="assets/lib/datatables/datatables.net-buttons/js/buttons.colVis.min.js" type="text/javascript"></script>
+    <script src="assets/lib/datatables/datatables.net-buttons/js/buttons.print.min.js" type="text/javascript"></script>
+    <script src="assets/lib/datatables/datatables.net-buttons/js/buttons.html5.min.js" type="text/javascript"></script>
+    <script src="assets/lib/datatables/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js" type="text/javascript"></script>
+    <script src="assets/lib/datatables/datatables.net-responsive/js/dataTables.responsive.min.js" type="text/javascript"></script>
+    <script src="assets/lib/datatables/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js" type="text/javascript"></script>
+    <script type="text/javascript">
+      $(document).ready(function(){
+      	//-initialize the javascript
+      	App.init();
+      	App.dataTables();
+      });
+    </script>
+  </body>
+
+</html>
